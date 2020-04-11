a5d73a74845cdc1cf2ed38752064ee6ffd06f5bc
nir vaknin <nirviko@gmail.com>
2020-04-11 18:40:15 +0300

    update4

--- test/unit test/sum.test.js
+++ test/unit test/sum.test.js
@@ -1,7 +1,5 @@
-const sum1 = require('./sum')
+const sum = require('./sum');
 
-
-test('adds',()=>  {
-    expect(sum1(4,2)).toBe(6)
-
-})
+test('adds 1 + 2 to equal 3', () => {
+  expect(sum(1, 2)).toBe(3);
+});
